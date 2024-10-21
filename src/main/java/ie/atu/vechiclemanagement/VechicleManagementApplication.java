f0998f69c9b330f020b2d3157ad64db802e5a0e0
G00401655@atu.ie <G00401655@atu.ie>
2024-10-21 16:07:37 +0100

    firstPush

---
+++ src/main/java/ie/atu/vechiclemanagement/VechicleManagementApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.vechiclemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VechicleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VechicleManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/vechiclemanagement/VechicleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.vechiclemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VechicleManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
